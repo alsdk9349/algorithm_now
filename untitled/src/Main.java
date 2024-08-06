2a8bb25852446caefc9a6471a5871b13f7bf6cfe
김민아 <alsdk9349@naver.com>
2024-08-06 10:27:52 +0900

    baekjoon2644

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,28 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int count = 0;
-        int total = 0;
-        for (int i=0;i<20;i++) {
+        n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(br.readLine());
+        arr = new int[n+1][n+1];
+        for (int i=0;i<m;i++) {
             st = new StringTokenizer(br.readLine());
-            String name = st.nextToken();
-            int point = Integer.parseInt(st.nextToken());
-            count+=point;
-            String score = st.nextToken();
-            if (score=="A+") {
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr[x][y] = 1;
+            arr[y][x] = 1;
+        }
+        bfs(a, b);
 
+    }
+    static int n,m,a,b;
+    static int [][] arr;
+    static int count = 0;
+    static void bfs(int start, int end) {
+        Queue<Map<Integer,Integer>> q = new LinkedList<>();
+        q.offer();
+        while(!q.isEmpty()) {
+            int now = q.poll();
+            if (now==end) {
+                break;
             }
-
+            for (int i=0;i<n+1;i++) {
+                if (arr[now][i])
+            }
         }
     }
 }
