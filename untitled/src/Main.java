7febc1c3f4134f2232453b1923ffe8f6797de72d
김민아 <alsdk9349@naver.com>
2024-08-13 10:17:23 +0900

    테스트케이스하나수정

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,51 +4,78 @@ import java.util.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
-        visited = new boolean[N][N];
-        resultarr = new ArrayList<>();
-        String s;
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        // 배열 입력 받기
+        arr = new int[N][M];
         for (int i=0;i<N;i++) {
-            s = br.readLine();
-            for (int j=0;j<N;j++) {
-                arr[i][j] = s.charAt(j)-'0';
+            st = new StringTokenizer(br.readLine());
+            for (int j=0;j<M;j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        bfs();
+        System.out.println(result);
+
+    }
+    static int M, N;
+    static int [][] arr;
+    static int [] directy = new int [] {0,0,1,-1};
+    static int [] directx = new int [] {1,-1,0,0};
+    static int result;
+
+    //처음부터 모든 토마토가 익어있는지 확인할 변수
+    static boolean flag = false;
+
+    static void bfs() {
+        int [] now;
+        int nexty,nextx,nextc;
+        Queue<int[]> q = new LinkedList<>();
+
+        // q안에 처음부터 익어있는 토마토 다 넣기
         for (int i=0;i<N;i++) {
-            for (int j=0;j<N;j++) {
-                if (arr[i][j]==1) {
-                    result++;
-                    bfs(i,j, 1);
+            for (int j=0;j<M;j++) {
+                if (arr[i][j] ==1) {
+                    q.offer(new int[]{i,j,0});
+                    flag = true;
                 }
             }
         }
-        System.out.println(result);
-    }
-    static int N;
-    static int[][]arr;
-    static int result = 0;
-    static ArrayList<Integer> resultarr;
-    static boolean [][] visited;
-    static int [] directy = new int [] {0,0,1,-1};
-    static int [] directx = new int [] {1,-1,0,0};
-    static void bfs(int y, int x, int count){
-        Queue<int[]> q = new LinkedList<>();
-        visited[y][x] = true;
-        arr[y][x] = 0;
-        q.offer(new int[]{y,x,count});
-        while(!q.isEmpty()) {
-            int [] now = q.poll();
-            int nowy = now[0];
-            int nowx = now[1];
-            int nowcount = now[2];
+
+        // q에 하나도 안들어갔을경우 바로 return
+        if (!flag) {
+            result = 0;
+            return;
+        }
+
+        while (!q.isEmpty()) {
+            now = q.poll();
             for (int i=0;i<4;i++) {
-                int nexty = nowy+directy[i];
-                int nextx = nowx+directx[i];
-                if ( 0<= nexty && 0<= nextx && nexty<N && nextx<N && !visited[nexty][nextx] && arr[nexty][nextx]==1) {
-                    q.offer(new int[]{nexty,nextx,nowcount+1});
+                nexty = now[0] + directy[i];
+                nextx = now[1] + directx[i];
+                if ( 0<=nexty && nexty<N && 0<=nextx && nextx<M && arr[nexty][nextx]==0) {
+                    arr[nexty][nextx] = 1;
+                    q.offer(new int[]{nexty,nextx,now[2]+1});
+                    result = now[2]+1;
+                }
+            }
+        }
+
+        // 토마토가 모두 익었는지 확인
+        for (int i=0;i<N;i++) {
+            for (int j=0;j<M;j++) {
+                if (arr[i][j]==0) {
+                    result = -1;
                 }
             }
         }
     }
 }
+
+//2 2
+//0 0
+//0 0
+//일때 -1이어야함
